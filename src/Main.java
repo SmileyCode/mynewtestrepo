8ef16536858375c4d96d2eeee5ebc09ec28b9d5e
SmileyCode <lukindimik2@mail.ru>
2023-10-10 19:15:21 +0300

    My first commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    //Вт, Пт в 19:00
+
+    //Java-Разработка
+    //Web-разработка -> Spring
+    //Android-разработка -> Kotlin
+    //Автотесты -> Selenium, Appium
+    //.jar
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //Тип название = значение
+        String a = "Сумма = ";
+        Double c = 1.1;
+        boolean d = true;//true / false
+        Character e = 'a';
+
+        Integer b = 1;
+        Integer b2 = 5;
+        Integer sum = b + b2;
+        System.out.println("Результат = " + a + sum);
+
+        Integer chislo = 5;
+        Integer stepen = 3;
+        Double result = Math.pow(5, 3);
+        Double pi = Math.PI;
+        System.out.println("5 в 3 степени = " + result);
+        System.out.println("Число Pi = " + pi);
+
+    }
+
+}
